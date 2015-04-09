443a7b89365b84321a5fbe97ddbb586627e05dae
Lynchy <lynchy95charles@gmail.com>
2015-04-09 15:51:37 +0100

    Mongo Category class created

---
+++ TaskTracker/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TaskTracker
+{
+    public class Category
+    {
+        public Category(string category,DateTime date)
+        {
+            CategoryName = category;
+            DateStamp = date;            
+        }
+
+        [BsonId]
+        public MongoDB.Bson.ObjectId _id { get; set; }
+
+        [BsonElement]
+        public string CategoryName { get; set; }
+
+        [BsonDateTimeOptions]
+        public DateTime DateStamp { get; set; }
+    }
+}
---
+++ TaskTracker/TaskContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace TaskTracker
+{
+    public class TaskContext
+    {
+        static async Task ConnectToDB() 
+        {
+            
+        }
+    }
+}
